f43542bccb88a14b8230f9a33d9e80cd9121f2d8
Solo <juan@xmartlabs.com>
2023-12-08 16:44:19 -0300

    integrates drag and drop and adds logic to generate code from file

--- app/page.tsx
+++ app/page.tsx
@@ -4,87 +4,46 @@
 // https://cdn.businessinsider.es/sites/navi.axelspringer.es/public/media/image/2023/01/instagram-notes-2942288.jpg?tf=2048x
 
 import { Form } from '@/app/form'
+import { Step, useImageToCode } from '@/app/useImageToCode'
+import { DragAndDrop } from '@/components/ui/dragAndDrop'
 import { Spinner } from '@/components/ui/spinner'
-import { useState } from 'react'
-
-enum Step {
-	Initial = 'Initial',
-	Loading = 'Loading',
-	Preview = 'Preview',
-	Error = 'Error',
-}
-
-async function* streamReader(res: Response) {
-	const reader = res.body?.getReader()
-	const decoder = new TextDecoder()
-	if (reader == null) return
-
-	while (true) {
-		const { done, value } = await reader?.read()
-		const chunk = decoder.decode(value)
-		yield chunk
-		if (done) break
-	}
-}
 
 export default function Home() {
-	const [result, setResult] = useState('')
-	const [step, setStep] = useState<Step>(Step.Initial)
+  const { genCodeFromImage, genCodeFromUrl, result, step } = useImageToCode()
 
-	const transformUrlToCode = async (url: string) => {
-		setStep(Step.Loading)
-		const res = await fetch('api/generate-code-from-image', {
-			method: 'POST',
-			body: JSON.stringify({ url }),
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		})
+  return (
+    <div className="grid grid-cols-[400px_1fr]">
+      <aside className="flex flex-col justify-between min-h-screen p-4 bg-gray-900">
+        <header className="text-center">
+          <h1 className="text-3xl font-semibold">Image to code</h1>
+          <h2 className="text-sm opacity-75">Convert your images to code in seconds</h2>
+        </header>
 
-		if (!res.ok || res.body === null) {
-			setStep(Step.Error)
-			throw new Error('Error generating the code')
-		}
+        <section>{/*filters*/}</section>
 
-		setStep(Step.Preview)
+        <footer>Developed by Solo</footer>
+      </aside>
 
-		for await (const chunk of streamReader(res)) {
-			setResult((prev) => prev + chunk)
-		}
-	}
-
-	return (
-		<div className="grid grid-cols-[400px_1fr]">
-			<aside className="flex flex-col justify-between min-h-screen p-4 bg-gray-900">
-				<header className="text-center">
-					<h1 className="text-3xl font-semibold">Image to code</h1>
-					<h2 className="text-sm opacity-75">Convert your images to code in seconds</h2>
-				</header>
-
-				<section>{/*filters*/}</section>
-
-				<footer>Developed by Solo</footer>
-			</aside>
-
-			<main className="bg-gray-950">
-				<section className="max-w-5xl w-full mx-auto p-10">
-					{step === Step.Loading && (
-						<div className="flex justify-center items-center h-full">
-							<Spinner />
-						</div>
-					)}
-					{step === Step.Preview && (
-						<div className="rounded flex flex-col gap-4">
-							<iframe srcDoc={result} className="w-full h-full border-4 rounded border-gray-700 aspect-video" />
-						</div>
-					)}
-					{step === Step.Initial && (
-						<div className="flex flex-col gap-10">
-							<Form transformUrlToCode={transformUrlToCode} />
-						</div>
-					)}
-				</section>
-			</main>
-		</div>
-	)
+      <main className="bg-gray-950">
+        <section className="max-w-5xl w-full mx-auto p-10">
+          {step === Step.Loading && (
+            <div className="flex justify-center items-center h-full">
+              <Spinner />
+            </div>
+          )}
+          {step === Step.Preview && (
+            <div className="rounded flex flex-col gap-4">
+              <iframe srcDoc={result} className="w-full h-full border-4 rounded border-gray-700 aspect-video" />
+            </div>
+          )}
+          {step === Step.Initial && (
+            <div className="flex flex-col gap-10">
+              <DragAndDrop transformImageToCode={genCodeFromImage} />
+              <Form transformUrlToCode={genCodeFromUrl} />
+            </div>
+          )}
+        </section>
+      </main>
+    </div>
+  )
 }
