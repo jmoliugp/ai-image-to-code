1d54df99138a09da67ed774bc39a850f4d2ac438
Solo <juan@xmartlabs.com>
2023-12-11 16:38:40 -0300

    import google fonts using next lib

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Space_Mono, Space_Grotesk } from 'next/font/google'
+
+export const spaceMono = Space_Mono({ subsets: ['latin'], weight: ['400'] })
+export const spaceMonoBold = Space_Mono({ subsets: ['latin'], weight: ['700'] })
+export const spaceGrotesk = Space_Grotesk({ subsets: ['latin'], weight: ['400'] })
