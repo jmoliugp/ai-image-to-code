dd776c9817e3e4eb80955cda5f470f64286fe7df
Solo <juan@xmartlabs.com>
2023-12-12 17:12:27 -0300

    fonts typography common module

--- app/page.tsx
+++ app/page.tsx
@@ -3,28 +3,22 @@
 // IG test screenshot
 // https://cdn.businessinsider.es/sites/navi.axelspringer.es/public/media/image/2023/01/instagram-notes-2942288.jpg?tf=2048x
 
-import Image from 'next/image'
-import { Form } from '@/app/form'
-import { Step, useImageToCode } from '@/app/useImageToCode'
-import { DragAndDrop } from '@/components/ui/dragAndDrop'
-import { Spinner } from '@/components/ui/spinner'
-import Head from 'next/head'
-import { spaceGrotesk, spaceMono, spaceMonoBold } from '@/app/ui/fonts'
-import { Dropzone } from '@/components/ui/dropzone'
+import { CodeGenProvider, useImageToCodeContext } from '@/app/providers/codeGenProvider'
+import { Step } from '@/app/useImageToCode'
+import CodePreview from '@/components/ui/codePreview'
+import ImageLoader from '@/components/ui/imageLoader'
 
-export default function Home() {
-  const { genCodeFromImage, genCodeFromUrl, result, step } = useImageToCode()
+function Home() {
+  const { step } = useImageToCodeContext()
 
+  const Scene = step === Step.Initial ? ImageLoader : CodePreview
+  return <Scene />
+}
+
+export default function HomeWithProvider() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen h-full relative bg-stone-800">
-      <div className="flex flex-row left-[771px] top-[276px] mb-20 text-center ">
-        <h1 className={`${spaceMonoBold.className}`}>IMAGE</h1>
-        <h1 className={`${spaceMono.className}`}>TO</h1>
-        <h1 className={`${spaceMonoBold.className}`}>CODE</h1>
-      </div>
-
-      <Dropzone />
-      <h2 className={`${spaceGrotesk.className} `}>Developed by Solo</h2>
-    </div>
+    <CodeGenProvider>
+      <Home />
+    </CodeGenProvider>
   )
 }
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,3 +3,10 @@ import { Space_Mono, Space_Grotesk } from 'next/font/google'
 export const spaceMono = Space_Mono({ subsets: ['latin'], weight: ['400'] })
 export const spaceMonoBold = Space_Mono({ subsets: ['latin'], weight: ['700'] })
 export const spaceGrotesk = Space_Grotesk({ subsets: ['latin'], weight: ['400'] })
+
+export const typography = {
+  h1: `${spaceMonoBold.className} text-stone-600 text-2xl font-bold tracking-widest`,
+  h2: `${spaceMono.className} text-stone-600 text-2xl font-bold tracking-widest`,
+  h3: `${spaceMonoBold.className} text-stone-600`,
+  p: `${spaceGrotesk.className} text-center text-stone-200`,
+}
